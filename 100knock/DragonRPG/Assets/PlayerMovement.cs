5d0271d3038afe97d645e448c07fc1b47cce9bd6
Yuto Uchida <kuronokurujp@gmail.com>
2017-11-24 15:03:39 +0900

    add DragonProject Sandbox

---
+++ 100knock/DragonRPG/Assets/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof(ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] float walkMoveStopRadius = 0.2f;
+
+    ThirdPersonCharacter m_Character;
+    CameraRaycaster cameraRaycaster;
+    Vector3 currentClickTarget;
+
+    // Use this for initialization
+    void Start () {
+
+        m_Character = GetComponent<ThirdPersonCharacter>();
+        cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+        currentClickTarget = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetMouseButton(0))
+        {
+            print("Cursor raycast hit" + cameraRaycaster.layerHit);
+            switch( cameraRaycaster.layerHit )
+            {
+                case Layer.Walkable:
+                    currentClickTarget = cameraRaycaster.hit.point;
+                    break;
+
+                case Layer.Enemy:
+                    print("Not moveing to enemy");
+                    break;
+
+                default:
+                    print("Unexpected layer found");
+                    return;
+            }
+        }
+
+        var playerToClickPoint = currentClickTarget - transform.position;
+        if(playerToClickPoint.magnitude >= walkMoveStopRadius )
+        {
+        }
+        else
+        {
+            playerToClickPoint = Vector3.zero;
+        }
+
+        m_Character.Move(playerToClickPoint, false, false);
+    }
+}
