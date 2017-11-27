0a6338c6ecae96f71a51f3f87d3f9129834bebbb
kuronokurujp <kuronokurujp@gmail.com>
2017-11-27 20:26:21 +0900

    completed section1 DragonRPG

---
+++ 100knock/DragonRPG/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof(ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] float walkMoveStopRadius = 0.2f;
+
+    ThirdPersonCharacter thirdPersonCharacter;
+    CameraRaycaster cameraRaycaster;
+    Vector3 currentClickTarget;
+
+    bool isInDirectMode = false;
+
+    // Use this for initialization
+    void Start () {
+
+        thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
+        cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+        currentClickTarget = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // 特定入力をするとマウスかキーボードでの入力方法を切り替え
+        // TODO add to menu
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            isInDirectMode = !isInDirectMode;
+            currentClickTarget = transform.position;
+        }
+
+        if (isInDirectMode)
+        {
+            ProcessDirectMovement();
+        }
+        else
+        {
+            ProcessMouseMovement();
+        }
+    }
+
+    private void ProcessDirectMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // calculate camera relative direction to move:
+        Vector3 camForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 movement = v * camForward + h * Camera.main.transform.right;
+
+        thirdPersonCharacter.Move(movement, false, false);
+    }
+
+    private void ProcessMouseMovement()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //print("Cursor raycast hit" + cameraRaycaster.layerHit);
+            switch (cameraRaycaster.currentLayerHit)
+            {
+                case Layer.Walkable:
+                    currentClickTarget = cameraRaycaster.hit.point;
+                    break;
+
+                case Layer.Enemy:
+              //      print("Not moveing to enemy");
+                    break;
+
+                default:
+               //     print("Unexpected layer found");
+                    return;
+            }
+        }
+
+        var playerToClickPoint = currentClickTarget - transform.position;
+        if (playerToClickPoint.magnitude >= walkMoveStopRadius)
+        {
+        }
+        else
+        {
+            playerToClickPoint = Vector3.zero;
+        }
+
+        thirdPersonCharacter.Move(playerToClickPoint, false, false);
+    }
+}
